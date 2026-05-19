7a696a703d6d48f60b6b5a2ba072820a19063e12
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-05-19 17:37:53 +0700

    feat(jsx): learning single root element

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    // 1. must return single root element
+    <div>
+      <h1>This is my first React Website</h1>
+      <p>Welcome to my place</p>
+    </div>
+  );
+}
+
+export default App;
